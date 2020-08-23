33e04def6f16a66aac3f8f34f64516d7e4574262
Ryan Lee <ryanm.lee11@gmail.com>
2020-08-23 02:32:30 +0000

    Challenge - Sum all numbers till the given one with 3 solutions

--- index.js
+++ index.js
@@ -36,3 +36,40 @@ function pow3(x, n) {
 }
 
 alert( pow3(2, 2) ); // 8
+
+//Challenge 1 - Sum all numbers till the given one
+
+// Write a function sumTo(n) that calculates the sum of numbers 1 + 2 + ... + n.
+
+//Solution 1 - Using a for loop.
+
+
+function sumTo( n) 
+{ 
+    var result = 0; // initialize result 
+    // One by one compute sum of digits in every number from 
+    // 1 to n 
+    for (var i = 1; i <= n; i++) 
+        result += i; 
+  
+    return result; 
+} 
+
+alert( sumTo(100) ); // 5050
+
+//Solution 2 - Using a recursion, cause sumTo(n) = n + sumTo(n-1) for n > 1.
+
+function sumTo2(n) {
+  if (n == 1) return 1;
+  return n + sumTo(n - 1);
+}
+
+alert( sumTo2(100) );
+
+//Solution 3 - Using the arithmetic progression formula.
+
+function sumTo3(n) {
+  return n * (n + 1) / 2;
+}
+
+alert( sumTo3(100) );
